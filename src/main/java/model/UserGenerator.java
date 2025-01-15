dffd721dc22db19cd3667f4b16e945c74baba638
Ilsina Komarova <ilsinchik@list.ru>
2025-01-15 20:37:31 +0300

    add UserGenerator

---
+++ src/main/java/model/UserGenerator.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.UUID;
+
+public class UserGenerator {
+    public static UserDataLombok getRandomUser() {
+        String email = "test-" + UUID.randomUUID() + "@yandex.ru";
+        String password = "password";
+        String name = "Username";
+        return new UserDataLombok(email, password, name);
+    }
+}
+
+
