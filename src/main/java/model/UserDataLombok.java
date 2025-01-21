ba2d5531465e865b016f32ab6f51678117cd6bf7
Ilsina Komarova <ilsinchik@list.ru>
2025-01-21 08:48:36 +0300

    add UserDataLombok

--- src/main/java/model/UserDataLombok.java
+++ src/main/java/model/UserDataLombok.java
@@ -2,7 +2,11 @@ package model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Data
 @AllArgsConstructor
 public class UserDataLombok {
